889097707d84bb430a3d3c819a7f30cb33d58b4c
nickjoegray <nickjoegray@gmail.com>
2015-02-01 14:19:14 -0500

    Setup subscription to chat channel and display connection status

--- app/app.js
+++ app/app.js
@@ -4,4 +4,35 @@ angular.module('chatApp', [])
 
 .controller('ChatCtrl', function($scope, $http) {
 
+   $scope.chatChannel = "chatApp";
+   $scope.messageLimit = 50;
+   $scope.defaultUsername = "Guest";
+
+   $scope.currentConnectionStatus = 0;
+
+   PUBNUB.subscribe({
+      channel: $scope.chatChannel,
+      restore: false,
+
+      callback: function() { },
+
+      disconnect: function() {   
+         $scope.$apply(function(){
+            $scope.currentConnectionStatus = 0;
+         });
+      },
+
+      reconnect: function() {   
+         $scope.$apply(function(){
+            $scope.currentConnectionStatus = 1;
+         });
+      },
+
+      connect: function() {
+         $scope.$apply(function(){
+            $scope.currentConnectionStatus = 2;
+         });
+      }
+   });
+
 });
